31ebcd84576c99822ff0b8dedf46d8ce466e2b9a
sultan wasif <sultanwasif@gmail.com>
2020-09-09 10:43:19 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CustomerComponent } from './customer/customer.component';
+import { ClientComponent } from './client/client.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: CustomerComponent
+      },
+      {
+        path: 'client',
+        component: ClientComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
